60c2e513f374d1168b7496c2191b5dfab150b0ec
Mary Elizabeth Shoop <maryelizabethshoop@Marys-MBP.wireless.davidson.edu>
2024-08-27 10:47:47 -0400

    Changed from development to production

--- app.py
+++ app.py
@@ -246,5 +246,5 @@ def download_csv():
 
     
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True)
